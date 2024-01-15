3e538f29b4773763dc9f4cbbf59f1ed24ad152ba
Sudhanshu9491 <148893552+Sudhanshu9491@users.noreply.github.com>
2024-01-15 22:53:17 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+let logIn = document.querySelector(".searchbar-cart-items");
+logIn.addEventListener('click', () => {
+    window.location.href = "loginPage.html";
+});
+
+// // Example JavaScript for the login popup on your specific login page
+// function toggleLoginPopup() {
+//     var loginPopup =  document.querySelector(".searchbar-cart-items");
+//     loginPopup.classList.toggle("show");
+// }
+// toggleLoginPopup();
+
